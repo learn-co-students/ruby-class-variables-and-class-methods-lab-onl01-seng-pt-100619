27b0fbd3439584558754b92240b0a283468c082c
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-27 06:28:20 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+ @@count = 0
+ @@artists = []
+ @@genres = []
+ @@genre_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+    #@@genre_count << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genre_count
+    #binding.pry
+
+  end
+
+  def self.artist_count
+  end
+end
