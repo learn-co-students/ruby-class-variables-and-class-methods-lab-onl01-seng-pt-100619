89cfe0db6ed7a483c77898a09c9c13b0ca1b032e
allison cortez <allizetorc@gmail.com>
2019-10-29 23:41:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,42 @@
 class Song 
   
+   @@artists = []
+   @@genres = []
+   @@count = 0 
+   
   attr_accessor :name, :artist, :genre
-  
+   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
+    @genre = genre
     
     @@count +=1
     
-    @@genres << genre
+    # add the genre of the song being created to the @@genres array.
+    # All genres should be added to the array
+    @@artists << @artist
+    @@genres << @genre
+    
   end
-  
-  @@count = 0 
-  @@genres = []
   
   def self.count
     @@count
   end
   
+  def self.artists
+    @@artists.uniq
+  end
+  
   def self.genres
-    @@genres
+    @@genres.uniq 
+  end
+  
+#   def to_histogram
+# 	 inject(Hash.new(0)) { |h, x| h[x] += 1; h}
+# 	 end
+# 	end
+  
+  def self.genre_count
   end
 end
